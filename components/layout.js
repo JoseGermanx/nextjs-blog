03f494b6dff8b61a2377a45d807e622c79c5ce53
Jose G Martinez <jgermanmm@gmail.com>
2022-11-24 21:44:35 -0300

    Revisión de titulo

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'JGx';
-export const siteTitle = 'A simple personal blob de jgxdev in Next.js';
+export const siteTitle = 'Personal blog de jgxdev in Next.js';
 
 export default function Layout({ children, home }) {
   return (
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Making my fist blog on Next.js"
         />
         <meta
           property="og:image"
