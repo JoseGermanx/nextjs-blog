e5c56ddd7c418465e1d388feed78570a2b3a67ad
Jose G Martinez <jgermanmm@gmail.com>
2022-11-24 23:11:06 -0300

    Incorporación de funcionalidad email

---
+++ pages/api/email.js
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { SMTPClient } from "emailjs";
+
+export default function handler(req, res) {
+  const { email } = req.body;
+  //console.log(process.env)
+
+  const client = new SMTPClient({
+    user: 'josegmartinez.rsa@gmail.com',
+    password: 'm15126376*',
+    host: "smtp.gmail.com",
+    ssl: true,
+  });
+
+  try {
+    client.send({
+      text: `Hola, necesito ponerme en contacto contigo, JG`,
+      from: email,
+      to: 'jgermanmm@gmail.com',
+      subject: "Contacto desde web site (nextJS)",
+    });
+  } catch (e) {
+    res.status(400).end(JSON.stringify({ message: "Error" }));
+    return;
+  }
+
+  res.status(200).end(JSON.stringify({ message: "Send Mail" }));
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import { useState } from "react";
+import axios from "axios";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -17,17 +19,43 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
+  const [email, setEmail] = useState("");
+  const SendMail = async (e) => {
+    e.preventDefault();
+    console.log("call");
+    axios.post('http://localhost:3000/api/email',{email})
+   .then(
+   (res)=>{
+     alert('Mensaje enviado')
+     setEmail('')
+   }
+   ).catch(
+     (e)=>console.log(e)
+   )
+  };
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      <main className={utilStyles.headingMd}>
+        <p>Ingresa tu email y me pongo en contacto contigo</p>
+       <form>
+      <input type="email" placeholder="Ingresa tu email" required value={email} onChange={(e)=>setEmail(e.target.value)}></input>
+      <button onClick={SendMail}>Send</button>
+        </form>
+     </main>
       <section className={utilStyles.headingMd}>
-        <p>Full Stack Web Developer ➝ JavaScript : ✓, React: ✓, Redux : ✓, NodeJS: ✓, Express: ✓, PostgreSQL: ✓, Sequelize: ✓, MongoDB: ✓, <strong>NextJS: ✓</strong><br />
-        Follow me on{''} <a href="https://twitter.com/jgxdev">Twitter</a>
+        <p>
+          Full Stack Web Developer ➝ JavaScript : ✓, React: ✓, Redux : ✓,
+          NodeJS: ✓, Express: ✓, PostgreSQL: ✓, Sequelize: ✓, MongoDB: ✓,{" "}
+          <strong>NextJS: ✓</strong>
+          <br />
+          Follow me on{""} <a href="https://twitter.com/jgxdev">Twitter</a>
         </p>
         <p>
-          (This web site is a part of my work on front-end development, here is the repositorie{' '}
+          (This web site is a part of my work on front-end development, here is
+          the repositorie{" "}
           <a href="https://github.com/jgxdev/nextjs-blog">on GitHub</a>.)
         </p>
       </section>
@@ -37,12 +65,12 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>{title}</Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
