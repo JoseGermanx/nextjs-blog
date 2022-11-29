eb65f9dcb3273a6e7860f096237a399de94622bb
Jose G Martinez <jgermanmm@gmail.com>
2022-11-29 13:36:22 -0300

    Cambio de tamaño de fuente de texto de footer

--- components/Presentacion.js
+++ components/Presentacion.js
@@ -3,7 +3,7 @@ import utilStyles from "../styles/utils.module.css";
 
 export default function Presentacion() {
   return (
-    <div className={utilStyles.headingMd}><p>
+    <div className={utilStyles.footer}><p>
     (Este sitio web es parte de la muestra de mi trabajo como desarrollador front-end, y puedes
     ver el código fuente en el repositorio{" "}
     <a href="https://github.com/jgxdev/nextjs-blog">en github</a>.)
