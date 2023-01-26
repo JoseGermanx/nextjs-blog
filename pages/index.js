1a30d5d608acdc435cc8fb10f453f6279f021661
Jose Germán Martínez <jgermanmm@gmail.com>
2023-01-26 17:39:07 -0300

    Actualizar index.js
    Se agrega link de github

--- pages/index.js
+++ pages/index.js
@@ -32,6 +32,8 @@ export default function Home({ allPostsData }) {
         React: ✓, Redux : ✓, NodeJS: ✓, Express: ✓, PostgreSQL: ✓, Sequelize:
           ✓, MongoDB: ✓, <strong>NextJS: ✓</strong>
           <br />
+          View my work on {""} <a href="https://hithub.com/josegermanx">GitHub</a>
+          <br />
           Follow me on{""} <a href="https://twitter.com/jgxdev">Twitter</a>
         </p>
       </section>
