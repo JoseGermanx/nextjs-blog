0ddf6b745cff982cc1f045567ce1ada7551b0408
Jose Germán Martínez <jgermanmm@gmail.com>
2023-03-15 10:17:11 -0300

    Se incluyen detalles de proyectos

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -11,12 +11,14 @@ export default function Portfolio() {
             <li><div>
                 <h3>Dionisio Wines</h3>
                 <p>Web app desarrollada en ReactJS con un back-end hecho en nodeJS y express. Sitio web e-commerce de vinos en Argentina.</p>
-                <a href="https://dionisiowines.onrender.com/" target="_blank" rel="noopener noreferrer"></a>
+                <p><strong>Tecnologías y características: </strong> React: ✓, Redux : ✓, NodeJS: ✓, Express: ✓, PostgreSQL: ✓, Sequelize: ✓, Auth: ✓, APIRest:✓, Integración de pagos on line: ✓, Registro de usaurios: ✓, Carro de compras: ✓</p>
+                <a href="https://dionisiowines.onrender.com/" target="_blank" rel="noopener noreferrer">Ver el proyecto on line</a>
                 </div></li>
                 <li><div>
                 <h3>CountriesApp</h3>
                 <p>App SPA desarrollada en ReactJS con un back-end hecho en nodeJS y express. Consumo de API y manejo de bases de datos.</p>
-                <a href="https://countriespi-pro.onrender.com/" target="_blank" rel="noopener noreferrer"></a>
+                <p><strong>Tecnologías y características: </strong> React: ✓, Redux : ✓, NodeJS: ✓, Express: ✓, PostgreSQL: ✓, Sequelize: ✓, Auth: ✓, APIRest:✓</p>
+                <a href="https://countriespi-pro.onrender.com/" target="_blank" rel="noopener noreferrer">Ver el proyecto on line</a>
                 </div></li>
             </ul>
             </div>
